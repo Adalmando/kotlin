f8bdd2f44597f12a1572bb55454ab593f1d9d6b2
Adalmando da Silva Araújo <adalmando.silva@phoebus.local>
2023-05-11 17:51:23 -0300

    nada

--- kotlin/src/main/kotlin/funcoes/Exercicio.kt
+++ kotlin/src/main/kotlin/funcoes/Exercicio.kt
@@ -1,16 +1,5 @@
 package funcoes
 
-import java.util.Date
-
-fun main(){
-
-    transformaAnos(2)
-    println(contaCaracteres("adalmando"))
-    println(cubo(3))
-    println(converteMilhas(50.0 ))
-
-}
-
 // 1. Escreva uma função que seja capaz de receber a quantidade de anos e transformar
 //em meses, dias, horas, minutos e segundos. Saída desejada:
 // 2 anos equivalem a:
@@ -21,12 +10,12 @@ fun main(){
 // 63072000 segundos
 
 fun transformaAnos(anos: Int){
-    println("$anos anos equivalem a:\n" +
-            "${anos*12} meses\n" +
-            "${anos*365} dias\n" +
-            "${anos*8766} horas\n" +
-            "${anos*525960} minutos\n" +
-            "${anos*31536000} segundos\n")
+    println("$anos anos equivalem a:")
+    println("${anos*12} meses")
+    println("${anos*365} dias")
+    println("${anos*365*24} horas")
+    println("${anos*365*24*60} minutos")
+    println("${anos*365*24*60*60} segundos")
 }
 
 // 2. Escreva uma função capaz de receber uma string e retornar a quantidade de caracteres.
@@ -36,4 +25,17 @@ fun contaCaracteres(entrada: String) = entrada.length
 fun cubo(numero: Int) = numero*numero*numero
 
 // 4. Escreva uma função capaz de receber milhas e converter em km (1 milha = 1,6km).
-fun converteMilhas(entrada: Double) = entrada*1.609
+fun converteMilhas(entrada: Float) = entrada*1.609f
+
+// 5. Escreva um programa que seja capaz de receber uma string e fazer a troca de todas as letras "a" ou "A" por "x".
+fun parseA(valor: String){
+    println("${valor.lowercase().replace('a', 'x')}")
+}
+fun main(){
+
+    transformaAnos(2)
+    println(contaCaracteres("adalmando"))
+    println(cubo(3))
+    println(converteMilhas(100f ))
+
+}
